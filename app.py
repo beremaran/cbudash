3741aa04447c264196e759620814a046c060dd89
Berke Emrecan Arslan <beremaran@gmail.com>
2018-10-28 16:11:17 +0300

    results now cached

--- app.py
+++ app.py
@@ -1,23 +1,59 @@
 #!/usr/bin/env python3
+import json
+import redis
 
+from celery.schedules import crontab
 from flask import Flask
 from flask import jsonify, render_template
 
 from cbudash.dash import CBUDash
+from cbudash.tasks import make_celery
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+application.config['CELERY_BROKER_URL'] = 'redis://redis:6379'
+application.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379'
+
+celery = make_celery(application)
+redis_db = redis.StrictRedis('redis', port=6379, db=0)
 cbu_dash = CBUDash()
 
 
-@app.route('/')
+def get_news():
+    news = redis_db.get('cbu_news')
+    if news is None:
+        news = cbu_dash.get(True)
+        update_cache.delay()
+    else:
+        news = json.loads(news.decode('utf-8'))
+
+    return news
+
+
+@application.route('/')
 def index():
-    return render_template('index.html', news=cbu_dash.get())
+    return render_template('index.html', news=get_news())
 
 
-@app.route('/feed.json')
+@application.route('/feed.json')
 def feed():
-    return jsonify(cbu_dash.get())
+    return jsonify(get_news())
+
+
+@celery.on_after_configure.connect()
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour='*', minute='*'),
+        update_cache.s()
+    )
+
+
+@celery.task()
+def update_cache():
+    print('Refreshing news sources')
+    redis_db.set('cbu_news', json.dumps(cbu_dash.get(True)))
+    print('News sources are refreshed.')
 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
